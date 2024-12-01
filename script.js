23943e074aad6d514d14b79b0c152e8c6d75c05d
Darya Kauzova <amayasuzuki12@gmail.com>
2024-12-01 17:42:54 +0300

    homework1

---
+++ script.js
@@ -0,0 +1,9 @@
+let celsius = 15;
+let fahrenheit = (9 / 5 * celsius) + 32;
+console.log(`Температура в градусах Цульсиях: ${celsius} 
+Температура в градусах Френгейтах: ${fahrenheit}`);
+
+let userName = 'Jess';
+let admin = userName;
+
+console.log(`Привет, ${admin}!`);
