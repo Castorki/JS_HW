8f2462e30f7f31d96c8cb5aa64e9c07b0f154f11
Darya Kauzova <amayasuzuki12@gmail.com>
2024-12-12 13:46:10 +0300

    homework 4

---
+++ HW4/script.js
@@ -0,0 +1,49 @@
+// for (let i = 0; i <= 11; i++) {
+//     if (i === 0) {
+//         console.log(i + ' - Это ноль');
+//     } else if (i % 2 === 0) {
+//         console.log(i + ' - Это четное число');
+//     }else{
+//         console.log(i + ' - Это нечетное число');
+//     }
+
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(arr);
+
+// arr.splice(arr.indexOf(4), 1);
+// arr.splice(arr.indexOf(5), 1);
+
+
+// console.log(arr);
+
+// const arr = [];
+
+// for (let i = 0; i < 5; i++) {
+//     arr.push(Math.floor(Math.random() * (10 - 0)));
+// }
+// console.log(arr);
+
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// let min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }
+// }
+// console.log(min);
+
+// console.log(arr.indexOf(3));
+
+for (let i = 1; i <= 20; i++) {
+    console.log('x'.repeat(i));;
+    
+}
